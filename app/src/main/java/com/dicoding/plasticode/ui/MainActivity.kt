697cf227112a15fe5a8980c5089a3fa1e0a13328
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-05-23 23:27:44 +0700

    add dashboard activity

--- app/src/main/java/com/dicoding/plasticode/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/plasticode/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dicoding.plasticode
+package com.dicoding.plasticode.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.plasticode.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
