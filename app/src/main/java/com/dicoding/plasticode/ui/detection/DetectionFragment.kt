303105bbf2a2f8a071969de4cc7eb3d0853ea8cb
Vicry Fahreza[ <vfahreza49@gmail.com>
2023-06-01 22:07:53 +0700

    Add CameraX Feature to DetectionFragment.kt

--- app/src/main/java/com/dicoding/plasticode/ui/detection/DetectionFragment.kt
+++ app/src/main/java/com/dicoding/plasticode/ui/detection/DetectionFragment.kt
@@ -1,23 +1,69 @@
 package com.dicoding.plasticode.ui.detection
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.dicoding.plasticode.R
 import com.dicoding.plasticode.databinding.FragmentDetectionBinding
 import com.dicoding.plasticode.ui.dashboard.activity.DashboardActivity
+import com.dicoding.plasticode.ui.detection.camera.CameraActivity
+import com.dicoding.plasticode.utils.rotateFile
+import java.io.File
 
 
 class DetectionFragment : Fragment() {
     private var _binding: FragmentDetectionBinding? = null
     private val binding get() = _binding!!
+    private var getFile: File? = null
 
     private lateinit var baseActivity: DashboardActivity
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode == CAMERA_X_RESULT){
+            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                it.data?.getSerializableExtra("picture", File::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.data?.getSerializableExtra("picture")
+            } as? File
+
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            myFile?.let {
+                rotateFile(it, isBackCamera)
+                getFile = it
+                binding.previewImg.setImageBitmap(BitmapFactory.decodeFile(it.path))
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         baseActivity = activity as DashboardActivity
+
+        if (!allPermissionGranted()){
+            ActivityCompat.requestPermissions(
+                activity as DashboardActivity,
+                REQUIRED_PERMISSION,
+                REQUEST_CODE_PERMISSION
+            )
+        }
+
+
     }
 
     override fun onCreateView(
@@ -32,7 +78,35 @@ class DetectionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val cameraXButton = view.findViewById<Button>(R.id.camera_button)
+        cameraXButton.setOnClickListener { runCameraX() }
+    }
 
+    @Deprecated("Deprecated in Java", ReplaceWith(
+        "super.onRequestPermissionsResult(requestCode, permissions, grantResults)",
+        "androidx.fragment.app.Fragment"
+    )
+    )
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSION){
+            if (!allPermissionGranted()){
+                Toast.makeText(activity, "Izin tidak diberikan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(activity!!.baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun runCameraX() {
+        val intent = Intent(requireContext(), CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
     }
 
     private fun initObserver() {
@@ -41,5 +115,11 @@ class DetectionFragment : Fragment() {
 
     private fun initListener() {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSION = 10
     }
 }
