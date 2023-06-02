6c7cac2f3017a0295de6a5f4905eef24bd84619c
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-06-02 16:49:14 +0700

    clean code

--- app/src/main/java/com/dicoding/plasticode/ui/riwayat/RiwayatActivity.kt
+++ app/src/main/java/com/dicoding/plasticode/ui/riwayat/RiwayatActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.plasticode.R
 import com.dicoding.plasticode.databinding.ActivityRiwayatBinding
+import com.dicoding.plasticode.ui.dashboard.DashboardActivity
 
 class RiwayatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRiwayatBinding
+    private lateinit var baseActivity: DashboardActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRiwayatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnDeteksi.setOnClickListener {
+            DashboardActivity.start(this, "deteksi")
+        }
     }
 }
