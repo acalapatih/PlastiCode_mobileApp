b84c5fa81991b8264870b268c5e6bb5b99c54b3c
Vicry Fahreza[ <vfahreza49@gmail.com>
2023-06-06 12:34:16 +0700

    ADD Login And Register Layout

---
+++ app/src/main/java/com/dicoding/plasticode/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.plasticode.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.plasticode.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
