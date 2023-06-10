e835ae0d2b42b77dc24ce3565e488123a8a145ba
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-06-10 22:45:40 +0700

    add Rekomendasi Barang Pengganti Plastik

--- app/src/main/java/com/dicoding/plasticode/ui/detection/camera/CameraActivity.kt
+++ app/src/main/java/com/dicoding/plasticode/ui/detection/camera/CameraActivity.kt
@@ -32,11 +32,6 @@ class CameraActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.captureImage.setOnClickListener { takePhoto() }
-        binding.switchCamera.setOnClickListener {
-            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
-            else CameraSelector.DEFAULT_BACK_CAMERA
-            startCamera()
-        }
     }
 
     private fun takePhoto() {
