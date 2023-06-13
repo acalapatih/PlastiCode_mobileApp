5602bdc030a913a6b7781941bd4c760bcbaa330d
Vicry Fahreza[ <vfahreza49@gmail.com>
2023-06-13 15:15:48 +0700

    Update with pull Recomendation Item

--- app/src/main/java/com/dicoding/plasticode/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/dicoding/plasticode/ui/login/LoginViewModel.kt
@@ -6,7 +6,7 @@ import com.dicoding.plasticode.service.UserPreference
 import kotlinx.coroutines.launch
 
 class LoginViewModel(private val pref: UserPreference): ViewModel() {
-    
+
     fun saveUser(user: String) {
         viewModelScope.launch {
             pref.saveUser(user)
